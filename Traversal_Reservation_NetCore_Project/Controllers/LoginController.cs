dc41fd590f3bf07b814b9fa3555b97f03e74d06e
Cevdet Sedef <cevdettsedeff@gmail.com>
2023-03-16 04:03:30 +0300

    Code was edited for identity class.

---
+++ Traversal_Reservation_NetCore_Project/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Traversal_Reservation_NetCore_Project.Models;
+
+namespace Traversal_Reservation_NetCore_Project.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public  IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserRegisterViewModel userRegisterViewModel)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = userRegisterViewModel.Name,
+                Surname = userRegisterViewModel.Surname,
+                Email = userRegisterViewModel.Mail,
+                UserName = userRegisterViewModel.Username
+            };
+            if (userRegisterViewModel.Password == userRegisterViewModel.ConfirmPassword)
+            {
+                var result = await _userManager.CreateAsync(appUser, userRegisterViewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("SignIn");
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(userRegisterViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
--- Traversal_Reservation_NetCore_Project/Startup.cs
+++ Traversal_Reservation_NetCore_Project/Startup.cs
@@ -1,6 +1,10 @@
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +27,17 @@ namespace Traversal_Reservation_NetCore_Project
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>();
+            services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>();
             services.AddControllersWithViews();
+
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +55,7 @@ namespace Traversal_Reservation_NetCore_Project
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseAuthentication();
             app.UseRouting();
 
             app.UseAuthorization();
