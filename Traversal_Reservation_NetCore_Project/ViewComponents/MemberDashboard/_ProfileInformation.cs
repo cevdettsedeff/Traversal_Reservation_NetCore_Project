f6036737b88880c3cadc95c64cf0d63facc13e09
Cevdet Sedef <cevdettsedeff@gmail.com>
2023-03-26 03:40:37 +0300

    A dashboard panel has been prepared for the members.

---
+++ Traversal_Reservation_NetCore_Project/ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Traversal_Reservation_NetCore_Project.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation
+    {
+        public class _FeaturePartial : ViewComponent
+        {
+            private readonly UserManager<AppUser> _userManager;
+
+            public _FeaturePartial(UserManager<AppUser> userManager)
+            {
+                _userManager = userManager;
+            }
+
+            public async Task<IViewComponentResult> InvokeAsync()
+            {
+                var values = await _userManager.FindByNameAsync(User.Identity.Name);
+                ViewBag.memberName = values.UserName + " " + values.Surname;
+                ViewBag.memberPhone = values.PhoneNumber;
+                ViewBag.memberMail = values.Email;
+                return View();
+            }
+        }
+    }
+}
